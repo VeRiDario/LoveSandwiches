b0af6effb45670021c5e3dc2b85d29dd6d8792d9
Dario Velazquez <v.dario.developer@gmail.com>
2022-12-25 15:08:33 +0000

    add user request loop to repeat request for data untilo data provided is valid

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,15 +13,25 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('LoveSandwiches')
 
 def get_sales_data():
-    """ Get sales figures input form the user"""
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by comas.")
-    print("Example:10,20,30,40,50,60\n")
+    """
+     Get sales figures input form the user
+     """
 
-    data_str = input("Enter your data here: ")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by comas.")
+        print("Example:10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break 
+    return sales_data    
+
+    
 
 def validate_data(values):
     """
@@ -32,12 +39,18 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there asen't exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+    
+data =get_sales_data()
+print(data)
